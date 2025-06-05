62493c17ffc4d06692ccfca3eb62979e9e32c3fe
fishrot <hongyi.yu@outlook.com>
2025-06-05 12:16:29 -0700

    Fill gpu cache (#15)
    Fill cache with data to make it dirty before render in benchmark test
    
    * fill gpu cache with random data
    
    * dirty cache
    
    ---------
    
    Co-authored-by: Hongyi Yu <hongyi.yu@genesis-ai.company>

--- examples/perf_benchmark/benchmark.py
+++ examples/perf_benchmark/benchmark.py
@@ -137,6 +137,17 @@ def add_noise_to_all_cameras(scene):
             up=cam.up_all_envs + torch.rand((cam.n_envs, 3), device=cam.up_all_envs.device) * 0.002 - 0.001,
         )
 
+def fill_gpu_cache_with_random_data():
+    # 100 MB of random data
+    dummy_data =torch.rand(100, 1024, 1024, device="cuda")
+    # Make some random data manipulation to the entire tensor
+    dummy_data = dummy_data + 1
+    dummy_data = dummy_data * 2
+    dummy_data = dummy_data - 1
+    dummy_data = dummy_data / 2
+    dummy_data = dummy_data.abs()
+    dummy_data = dummy_data.sqrt()
+
 def run_benchmark(scene, benchmark_args):
     try:
         n_envs = benchmark_args.n_envs
@@ -146,12 +157,14 @@ def run_benchmark(scene, benchmark_args):
         scene.step()
         rgb, depth, _, _ = scene.batch_render()
 
+        # fill gpu cache with random data
+        fill_gpu_cache_with_random_data()
+
         # timer
         from time import time
         start_time = time()
 
         for i in range(n_steps):
-            add_noise_to_all_cameras(scene)
             rgb, depth, _, _ = scene.batch_render(force_render=True)
         
         end_time = time()
