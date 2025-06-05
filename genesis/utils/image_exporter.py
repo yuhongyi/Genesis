da9d2a0e50e0981a62975e84598d03ec59275191
Hongyi Yu <hongyi.yu@genesis-ai.company>
2025-06-05 15:21:10 -0700

    Optimize

--- genesis/utils/image_exporter.py
+++ genesis/utils/image_exporter.py
@@ -10,45 +10,45 @@ class FrameImageExporter:
             os.makedirs(export_dir)
         self.depth_clip_max = depth_clip_max
     
-    def _export_rgb(self, i_env, i_cam, i_step, rgb):
-        rgb = rgb[..., [2, 1, 0]].cpu().numpy()[i_env, i_cam]
+    def _export_frame_rgb_batch_cam(self, i_env, i_cam, i_step, rgb):
+        rgb = rgb[i_env, i_cam, ..., [2, 1, 0]].cpu().numpy()
         cv2.imwrite(f'{self.export_dir}/rgb_env{i_env}_cam{i_cam}_{i_step:03d}.png', rgb)
 
-    def _export_depth(self, i_env, i_cam, i_step, depth):
-        depth = depth.cpu().numpy()[i_env, i_cam]
+    def _export_frame_depth_batch_cam(self, i_env, i_cam, i_step, depth):
+        depth = depth[i_env, i_cam].cpu().numpy()
         depth = np.clip(depth, 0, self.depth_clip_max)
         depth_normalized = cv2.normalize(depth, None, 0, 255, cv2.NORM_MINMAX)
         cv2.imwrite(f'{self.export_dir}/depth_env{i_env}_cam{i_cam}_{i_step:03d}.png', depth_normalized.astype(np.uint8))
 
-    def export_rgb_and_depth(self, i_step, rgb=None, depth=None):
+    def export_frame_batch_cam(self, i_step, rgb=None, depth=None):
         def export_single_env_cam(args):
             i_env, i_cam, rgb, depth, i_step = args
             if rgb is not None:
-                self._export_rgb(i_env, i_cam, i_step, rgb)
+                self._export_frame_rgb_batch_cam(i_env, i_cam, i_step, rgb)
             if depth is not None:
-                self._export_depth(i_env, i_cam, i_step, depth)
+                self._export_frame_depth_batch_cam(i_env, i_cam, i_step, depth)
         
         args_list = [(i_env, i_cam, rgb, depth, i_step) for i_env in range(rgb.shape[0]) for i_cam in range(rgb.shape[1])]
         with Pool() as pool:
             pool.map(export_single_env_cam, args_list)
 
-    def _export_rgb_single_cam(self, i_env, i_step, cam_idx, rgb):
-        rgb = rgb[..., [2, 1, 0]].cpu().numpy()[i_env]
+    def _export_frame_rgb_single_cam(self, i_env, i_step, cam_idx, rgb):
+        rgb = rgb[i_env, ..., [2, 1, 0]].cpu().numpy()
         cv2.imwrite(f'{self.export_dir}/rgb_env{i_env}_cam{cam_idx}_{i_step:03d}.png', rgb)
 
-    def _export_depth_single_cam(self, i_env, i_step, cam_idx, depth):
-        depth = depth.cpu().numpy()[i_env]
+    def _export_frame_depth_single_cam(self, i_env, i_step, cam_idx, depth):
+        depth = depth[i_env].cpu().numpy()
         depth = np.clip(depth, 0, self.depth_clip_max)
         depth_normalized = cv2.normalize(depth, None, 0, 255, cv2.NORM_MINMAX)
         cv2.imwrite(f'{self.export_dir}/depth_env{i_env}_cam{cam_idx}_{i_step:03d}.png', depth_normalized.astype(np.uint8))
 
-    def export_rgb_and_depth_single_cam(self, i_step, cam_idx, rgb=None, depth=None):
+    def export_frame_single_cam(self, i_step, cam_idx, rgb=None, depth=None):
         def export_single_env(args):
             i_env, rgb, depth, i_step, cam_idx = args
             if rgb is not None:
-                self._export_rgb_single_cam(i_env, i_step, cam_idx, rgb)
+                self._export_frame_rgb_single_cam(i_env, i_step, cam_idx, rgb)
             if depth is not None:
-                self._export_depth_single_cam(i_env, i_step, cam_idx, depth)
+                self._export_frame_depth_single_cam(i_env, i_step, cam_idx, depth)
         
         args_list = [(i_env, rgb, depth, i_step, cam_idx) for i_env in range(rgb.shape[0])]
         with Pool() as pool:
