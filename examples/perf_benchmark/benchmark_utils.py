97b096443d26cff303f6a5901a31efa0c0e85c89
Hongyi Yu <hongyi.yu@genesis-ai.company>
2025-06-18 11:25:29 -0700

    Remove perf-benchmark sciprt. Will be merged in a separate PR

--- examples/perf_benchmark/benchmark_utils.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def fill_gpu_cache_with_random_data():
-    # 100 MB of random data
-    dummy_data = torch.rand(100, 1024, 1024, device="cuda")
-    # Make some random data manipulation to the entire tensor
-    dummy_data = dummy_data.sqrt()
